b58a23fd1c557454692c5faf74448a5d96679b72
Frank Yuan <xyuan@kabaminc.com>
2015-01-13 14:53:14 +0800

    add helper methods

---
+++ PAT/PATHelper.h
@@ -0,0 +1,73 @@
+//
+//  PATHelper.h
+//  PAT
+//
+//  Created by YuanXuan on 1/13/15.
+//  Copyright (c) 2015 felk. All rights reserved.
+//
+
+#ifndef PAT_PATHelper_h
+#define PAT_PATHelper_h
+
+#include <string>
+#include <fstream>
+#include <sstream>
+
+class PATHelper
+{
+public:
+    
+//    static std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
+//        std::stringstream ss(s);
+//        std::string item;
+//        while (std::getline(ss, item, delim)) {
+//            elems.push_back(item);
+//        }
+//        return elems;
+//    }
+    template <class T> static std::vector<T> &split(const std::string &s, char delim, std::vector<T> &elems) {
+        std::stringstream ss(s);
+        std::string item;
+        T value;
+        while (std::getline(ss, item, delim)) {
+            if (item.length() > 0)
+            {
+                std::stringstream strstream;
+                strstream.str(item);
+                strstream >> value;
+                elems.push_back(value);
+            }
+        }
+        while (!ss.eof())
+        {
+            
+        }
+        return elems;
+    }
+    
+    static std::vector<std::string> split(const std::string &s, char delim) {
+        std::vector<std::string> elems;
+        return split(s, delim, elems);;
+    }
+
+    
+    template <class T> static std::vector< std::vector<T> > parseToMatrix(std::istream& inStream, char delim)
+    {
+        std::vector< std::vector<T> > rtn;
+        std::string line;
+        while (!inStream.eof())
+        {
+            std::getline(inStream, line);
+//            inStream >> line;
+            if (line.length() > 0)
+            {
+                std::vector<T> element;
+                rtn.push_back(split(line, delim, element));
+            }
+            
+        }
+        return rtn;
+    }
+};
+
+#endif
--- PAT/main.cpp
+++ PAT/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "PATHelper.h"
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::string str = "Hello   World !";
+    std::cout << str << std::endl;
+    std::vector<std::string> rtn = PATHelper::split(str, ' ');
+    for (int i = 0; i < rtn.size(); ++i)
+    {
+        std::cout << rtn[i] << std::endl;
+    }
+    
+    std::stringstream ss;
+    ss << "1 2 3 4 5" << std::endl << "21 22 23 24 25";
+    std::vector< std::vector<int> > m = PATHelper::parseToMatrix<int>(ss, ' ');
+    
+    for (int i = 0; i < m.size(); ++i)
+    {
+        std::vector<int>& row = m[i];
+        for (int j = 0; j < row.size(); ++j)
+        {
+            std::cout << row[j] << "\t";
+        }
+        std::cout << std::endl;
+    }
     return 0;
 }
