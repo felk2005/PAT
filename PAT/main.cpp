7592f0af95ee10393c10a90bf9c095ba0f492212
Frank Yuan <xyuan@kabaminc.com>
2015-01-13 15:02:52 +0800

    finish helper

--- PAT/main.cpp
+++ PAT/main.cpp
@@ -13,19 +13,12 @@
 #include "PATHelper.h"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::string str = "Hello   World !";
-    std::cout << str << std::endl;
-    std::vector<std::string> rtn = PATHelper::split(str, ' ');
-    for (int i = 0; i < rtn.size(); ++i)
-    {
-        std::cout << rtn[i] << std::endl;
-    }
+
+    std::fstream inFile(argv[1]);
+    // for matrix input
+    std::vector< std::vector<int> > m = PATHelper::parseToMatrix<int>(inFile, ' ');
     
-    std::stringstream ss;
-    ss << "1 2 3 4 5" << std::endl << "21 22 23 24 25";
-    std::vector< std::vector<int> > m = PATHelper::parseToMatrix<int>(ss, ' ');
-    
+    // for test print
     for (int i = 0; i < m.size(); ++i)
     {
         std::vector<int>& row = m[i];
