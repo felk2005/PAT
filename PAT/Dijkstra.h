e6e8d01dab943fbadc7f497eec3f562e5d4015eb
Frank Yuan <xyuan@kabaminc.com>
2015-01-29 11:14:30 +0800

    template version but some mistakes exist

---
+++ PAT/Dijkstra.h
@@ -0,0 +1,100 @@
+//
+//  Dijkstra.h
+//  PAT
+//
+//  Created by YuanXuan on 1/29/15.
+//  Copyright (c) 2015 felk. All rights reserved.
+//
+
+#ifndef PAT_Dijkstra_h
+#define PAT_Dijkstra_h
+#include "stlheaders.h"
+
+using namespace std;
+
+template <class T> class Dijkstra
+{
+    
+private:
+    struct NodeInfo
+    {
+        WeightedGraphNode<T>* fromNode;
+        int distance;
+        NodeInfo()
+        :fromNode(NULL), distance(0x7fffffff)
+        {
+            
+        }
+    };
+
+    
+    set<WeightedGraphNode<T>*> unknownSet;
+    map<WeightedGraphNode<T>*, NodeInfo> nodeInfoMap;
+
+public:
+    vector<T> Solution(vector<WeightedGraphNode<T>*>& graph, int startId, int destId)
+    {
+        // Initialize
+        for (int i = 0; i < graph.size(); ++i)
+        {
+            unknownSet.insert(graph[i]);
+            nodeInfoMap[graph[i]] = NodeInfo();
+        }
+            
+        WeightedGraphNode<T>* start = graph[startId];
+        WeightedGraphNode<T>* dest = graph[destId];
+        
+        // Traverse
+        
+        nodeInfoMap[start].distance = 0;
+        queue<WeightedGraphNode<T>*> nodeQueue;
+        nodeQueue.push(start);
+        
+        while (nodeQueue.size() > 0)
+        {
+            WeightedGraphNode<T>* node = nodeQueue.front();
+            nodeQueue.pop();
+            unknownSet.erase(node);
+            NodeInfo currentInfo = nodeInfoMap[node];
+            const typename WeightedGraphNode<T>::NodeList& neighbours = node->GetNeighbours();
+            for (int i = 0; i < neighbours.size(); ++i)
+            {
+                pair<WeightedGraphNode<T>*, int> nodeWithWeight = neighbours[i];
+                if (unknownSet.find(nodeWithWeight.first) != unknownSet.end())
+                {
+                    nodeQueue.push(nodeWithWeight.first);
+                    NodeInfo& neighbourInfo = nodeInfoMap[nodeWithWeight.first];
+                    int newDistance = currentInfo.distance + nodeWithWeight.second;
+                    if (newDistance < neighbourInfo.distance)
+                    {
+                        neighbourInfo.distance = newDistance;
+                        neighbourInfo.fromNode = node;
+                    }
+                }
+            }
+            for (int i = 0; i < graph.size(); ++i)
+            {
+                NodeInfo info = nodeInfoMap[graph[i]];
+                cout << graph[i]->GetValue() << "\t" ;
+                cout << (unknownSet.find(graph[i]) == unknownSet.end() ? "TRUE" : "FALSE") << "\t\t" ;
+                cout << info.distance << "\t" << (info.fromNode == NULL ? -1 : info.fromNode->GetValue()) << endl;
+            }
+            cout << "====================" << endl;
+        }
+        
+        vector<T> result;
+        WeightedGraphNode<T>* node = dest;
+        
+        while (node != NULL)
+        {
+            result.push_back(node->GetValue());
+            node = nodeInfoMap[node].fromNode;
+        }
+        
+         
+         
+         return result;
+    }
+};
+
+#endif
--- PAT/main.cpp
+++ PAT/main.cpp
@@ -10,9 +10,54 @@
 #include "stlheaders.h"
 #endif
 #include "WeightedGraphNode.h"
+#include "Dijkstra.h"
+using namespace std;
+//template <class T> struct NodeInfo
+//{
+//    WeightedGraphNode<T>* fromNode;
+//    int distance;
+//    NodeInfo()
+//    :fromNode(NULL), distance(0x7fffffff)
+//    {
+//        
+//    }
+//};
 
 int main(int argc, const char * argv[]) {
     
 
+    ifstream filestream(argv[1]);
+    istream& stream = filestream;
+    
+    int nodeNum, edgeNum, startId, destId;
+    stream >> nodeNum >> edgeNum >> startId >> destId;
+    
+    // build graph
+    vector<WeightedGraphNode<int>* > nodeList;
+    for(int i = 0; i < nodeNum; ++i)
+    {
+        nodeList.push_back(new WeightedGraphNode<int>(i));
+    }
+    
+    // read edges and build expense map
+    map<pair<int, int>, int> expenseMap;
+    for (int i = 0; i < edgeNum; ++i)
+    {
+        int start, end, weight, cost;
+        stream >> start >> end >> weight >> cost;
+        nodeList[start]->AddNeightbour(nodeList[end], weight);
+        nodeList[end]->AddNeightbour(nodeList[start], weight);
+        expenseMap[make_pair(min(start, end), max(start, end))] = cost;
+    }
+
+//    
+//    set<WeightedGraphNode<int>*> unknownSet;
+//    map<WeightedGraphNode<int>*, NodeInfo<int> > nodeInfoMap;
+    
+    vector<int> result = (new Dijkstra<int>)->Solution(nodeList, startId, destId);
+    for (int i = 0; i < result.size(); ++i)
+    {
+        cout << result[i] << endl;
+    }
     return 0;
 }
